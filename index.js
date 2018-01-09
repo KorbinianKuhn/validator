af50b3e8485b8477085461146695bf60b0d48bc0
Korbinian Kuhn <korbiniankuhn@gmail.com>
2018-01-09 17:27:57 +0100

    Fix invalid import in index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ exports.ValidationError = require('./src/error');
 
 exports.ARRAY = require('./src/types/array');
 exports.BOOLEAN = require('./src/types/boolean');
+exports.DATE = require('./src/types/date');
 exports.ENUM = require('./src/types/enum');
 exports.FUNCTION = require('./src/types/function');
 exports.INTEGER = require('./src/types/integer');
 exports.NUMBER = require('./src/types/number');
+exports.OBJECT = require('./src/types/object');
 exports.REGEX = require('./src/types/regex');
-exports.SCHEMA = require('./src/types/schema');
+exports.REQUEST = require('./src/types/request');
 exports.STRING = require('./src/types/string');
